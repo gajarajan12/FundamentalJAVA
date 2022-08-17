55229fd9e2b67db70a5795582194c22c51b894c2
gajarajan12 <gajarajanak@gmail.com>
2022-08-17 14:36:28 +0530

    Changed

--- src/main/java/fundamentalbasic/corejava/App.java
+++ src/main/java/fundamentalbasic/corejava/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("name : gajarajan,iniya,ramasamy,jaya");
+        System.out.println("place : chinna seeragapadi");
+        System.out.println("district : salem ");
+       System.out.println("fridge");
+        
     }
 }
--- src/main/java/fundamentalbasic/corejava/Sample.java
+++ src/main/java/fundamentalbasic/corejava/Sample.java
@@ -4,5 +4,7 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("HELLO JAVA.sugfiwrw..");
 	System.out.println("gaja kanna manoj bro");
+	System.out.println("chinna seeragapadi,salem DT");
+	System.out.println("perumal kovil street");
 }
 }
